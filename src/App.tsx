92e326a012ef1ce74521c4c9081f66fdc1f6098c
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-09-04 19:46:03 -0300

    feat: create Price component with styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Contradiction } from "./Components/Contradiction"
 import { Loop } from "./Components/Loop"
 import { Differential } from "./Components/Differential"
 import { About } from "./Components/About"
+import { Price } from "./Components/Price"
 
 import logoVc from "./assets/svg/logo-vcusa.svg"
 
@@ -26,6 +27,8 @@ export function App() {
       description="Muitos brasileiros que chegam aos Estados Unidos enfrentam a mesma barreira: falta de documentos, crédito limitado e a dificuldade de conquistar estabilidade logo no começo da vida. Mas a verdade é que isso não precisa ser um obstáculo.
       Nós desenvolvemos um método exclusivo pensado para quem está começando agora nos EUA e deseja construir um futuro com mais segurança e oportunidades. Um passo a passo simples, direto e comprovado, que mostra como superar as limitações e abrir portas que antes pareciam fechadas.
       Aqui, você não encontra apenas conhecimento técnico, mas uma nova forma de enxergar suas possibilidades. Você vai aprender estratégias práticas que podem ser aplicadas imediatamente, independentemente do seu nível de experiência ou da sua situação atual."/> 
+
+      <Price/>
     </div>
   )
 }
---
+++ src/Components/Price/index.tsx
@@ -0,0 +1,31 @@
+import styles from "./styles.module.css"
+
+import { Point } from "../Point"
+import { Button } from "../Button"
+import payments from "../../assets/svg/image-payments.svg"
+
+export function Price() {
+    return (
+        <div className={styles.container}>
+            <h2>Do Zero à Liberdade Financeira com Carros nos EUA</h2>
+
+            <div className={styles.divPrice}>
+                <div className={styles.points}>
+                    <Point name="Como participar de leilões de forma segura e legal."/>
+                    <Point name="Como comprar carros por valores muito abaixo da tabela."/>
+                    <Point name="Estratégias para revender ou usar o carro sem burocracia."/>
+                    <Point name="Passo a passo do zero, mesmo que você nunca tenha comprado antes."/>
+                </div>
+
+                <div className={styles.price}>
+                    <p>US$ 400</p>
+                    <Button name="COMPRAR AGORA"/>
+                </div>
+
+                <div className={styles.payments}>
+                    <img src={payments} alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
