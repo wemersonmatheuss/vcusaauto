60a7f01207ca6c1db99070e18eb0c6056c47a316
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-09-08 11:26:33 -0300

    feat: add number whatsapp

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,15 +1,28 @@
 import styles from "./styles.module.css"
 
 type Props = React.ComponentProps<"button"> & {
-    name: string
-    
-    onclick?: () => void
-}
+    name: string;
+};
 
-export function Button({ name, onclick, ...rest }: Props) {
+export function Button({ name, ...rest }: Props) {
+    
+    const handleClick = () => {
+        const phone = "17866893204";
+        const message = "Olá, quero saber mais sobre o curso";
+        const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+        window.open(url, "_blank"); // abre em nova aba
+    };
+
     return (
         <div>
-            <button {...rest} type="button" onClick={onclick} className={styles.container}>{name}</button>
+            <button 
+                {...rest} 
+                type="button"  
+                className={styles.container} 
+                onClick={handleClick}
+            >
+                {name}
+            </button>
         </div>
-    )
+    );
 }
